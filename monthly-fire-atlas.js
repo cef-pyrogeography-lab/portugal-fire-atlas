329598d1ba83922b3e3ed0be9ce2caccd93e1a6b
Alana K. Neves <43558098+akneves@users.noreply.github.com>
2023-03-23 16:12:13 +0000

    Update monthly-fire-atlas.js

--- monthly-fire-atlas.js
+++ monthly-fire-atlas.js
@@ -10,8 +10,7 @@
  * 
 */
 
-//Import Portugal limit
-//Feature collection
+//Import mainland Portugal shapefile as feature collection
 var PT_int = ee.FeatureCollection('users/alanakneves/Portugal-limit-continente');
 //Collect the feature collection geometry
 var geom = PT_int.geometry();
@@ -20,10 +19,11 @@ print(geom);
 
 Map.centerObject(geom, 6);
 
+//Start and end dates of analysis
 var start = ee.Date('2016-01-01');
 var end = ee.Date('2016-12-31');
 
-// FILTER IMAGES BY PERIOD AND BBOX 
+// FILTER LANDSAT 7 AND 8 IMAGES BY DATE, AREA OF INTEREST AND CLOUD COVER
 
 var filteredCollection_all_8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
   .filter(ee.Filter.bounds(geom))
@@ -38,7 +38,7 @@ var filteredCollection_all_7 = ee.ImageCollection('LANDSAT/LE07/C02/T1_L2')
 print('Filtered collection L8  - all year: ', filteredCollection_all_8);
 print('Filtered collection L7  - all year: ', filteredCollection_all_7);
 
-// APPLY FOCAL MEAN TO FILL L7 GAPS
+// APPLY FOCAL MEAN TO FILL L7 IMAGE GAPS
 
 var filtered_focal_mean = function(img) {
   var img_fill = img.focalMean(1, 'square', 'pixels', 8).blend(img);
@@ -49,20 +49,19 @@ var l7_filtered = filteredCollection_all_7.map(filtered_focal_mean);
 
 print('L7 after focal mean', l7_filtered);
 
-// CHECK L7 FILLED IMAGE
+// CHECK IF L7 GAP FILLING WORKED
 
 var list_l7 = filteredCollection_all_7.sort('system:time_start').toList(filteredCollection_all_7.size());
 print('List of all L7 images', list_l7);
 var l7_single = ee.Image(list_l7.get(51));
-Map.addLayer(l7_single, {bands: ['SR_B7', 'SR_B4', 'SR_B3'], min: 3000, max: 20000}, 'L7 ex before',false);
+Map.addLayer(l7_single, {bands: ['SR_B7', 'SR_B4', 'SR_B3'], min: 3000, max: 20000}, 'L7 before gap filling',false);
 
 var list_l7_filt = l7_filtered.sort('system:time_start').toList(l7_filtered.size());
 print('List of all L7 images after focal mean', list_l7_filt);
 var l7_single_ = ee.Image(list_l7_filt.get(51));
-Map.addLayer(l7_single_, {bands: ['SR_B7', 'SR_B4', 'SR_B3'], min: 3000, max: 20000}, 'L7 ex after',false);
+Map.addLayer(l7_single_, {bands: ['SR_B7', 'SR_B4', 'SR_B3'], min: 3000, max: 20000}, 'L7 after gap filling',false);
 
-
-//CREATE DATE BAND (DOY) FOR EACH IMAGE IN COLLECTION
+//CREATE A DOY BAND FOR EACH IMAGE IN COLLECTION
 
 var addDate = function(image){
   var doy = image.date().getRelative('day', 'year');
@@ -72,16 +71,17 @@ var addDate = function(image){
   return image.addBands(doyBand);
 };
 
-//apply it
+// Incorporate DOY bands into the original collections
+
 var withDate_l7 = l7_filtered.map(addDate).sort('system:time_start');
 print('L7 Collection with doy band: ', withDate_l7);
 
 var withDate_l8 = filteredCollection_all_8.map(addDate).sort('system:time_start');
 print('L8 Collection with doy band: ', withDate_l8);
 
-//RENAME L7 AND L8 BANDS TO MERGE THEM IN A UNIQUE COLLECTION
+//RENAME L7 AND L8 BANDS TO MERGE THEM IN AN UNIQUE COLLECTION
 
-//L7
+////L7
 var rename_l7 = function(img) {
     var select = img.select('SR_B3','SR_B4','SR_B7','ST_B6','doy','QA_PIXEL','QA_RADSAT')
                     .rename(['Red','NIR','SWIR2','Thermal','doy','QA_PIXEL','QA_RADSAT'])
@@ -90,15 +90,13 @@ var rename_l7 = function(img) {
 };
 
 var l7_renamed = withDate_l7.map(rename_l7);
-print('L7 renamed:', l7_renamed);
+print('L7 with renamed bands:', l7_renamed);
 
-//test it
 var list_l7_ren = l7_renamed.sort('system:time_start').toList(l7_renamed.size());
-print('List of all L7 images', list_l7_ren);
 var l7_ren_test = ee.Image(list_l7_ren.get(51));
-Map.addLayer(l7_ren_test, {bands: ['SWIR2', 'NIR', 'Red'], min: 3000, max: 20000}, 'L7 renamed',false);
+Map.addLayer(l7_ren_test, {bands: ['SWIR2', 'NIR', 'Red'], min: 3000, max: 20000}, 'L7 with renamed bands',false);
 
-//L8
+////L8
 var rename_l8 = function(img) {
     var select = img.select('SR_B4','SR_B5','SR_B7','ST_B10','doy','QA_PIXEL','QA_RADSAT')
                     .rename(['Red','NIR','SWIR2','Thermal','doy','QA_PIXEL','QA_RADSAT'])
@@ -107,23 +105,16 @@ var rename_l8 = function(img) {
 };
 
 var l8_renamed = withDate_l8.map(rename_l8);
-print('L8 renamed:', l8_renamed);
-
-//test it
-var list_l8_ren_bef = withDate_l8.sort('system:time_start').toList(withDate_l8.size());
-var l8_ren_test_bef = ee.Image(list_l8_ren_bef.get(10));
-Map.addLayer(l8_ren_test_bef, {bands: ['SR_B7', 'SR_B5', 'SR_B4'], min: 3000, max: 20000}, 'L8 renamed - before',false);
-
+print('L8 with renamed bands:', l8_renamed);
 
 var list_l8_ren = l8_renamed.sort('system:time_start').toList(l8_renamed.size());
-print('List of all L8 images', list_l8_ren);
 var l8_ren_test = ee.Image(list_l8_ren.get(10));
-Map.addLayer(l8_ren_test, {bands: ['SWIR2', 'NIR', 'Red'], min: 3000, max: 20000}, 'L8 renamed - after',false);
+Map.addLayer(l8_ren_test, {bands: ['SWIR2', 'NIR', 'Red'], min: 3000, max: 20000}, 'L8 with renamed bands',false);
 
-// MERGE L5 AND L7
+// MERGE L7 AND L8 COLLECTIONS
 
 var filteredCollection_all = l8_renamed.merge(l7_renamed).sort('system:time_start');
-print('Filtered collection - L7 and L8: ', filteredCollection_all);
+print('Filtered collection - L7 and L8 merged: ', filteredCollection_all);
 
 // CREATE MOSAICS WITH IMAGES FROM THE SAME DATE
 
@@ -177,11 +168,11 @@ var mean_doy = doycol.map(function(img){
 
 print('MEAN DOY', mean_doy);
 
-// COMBINE MEAN DOY COLLECTION WITH ORIGINAL ONE 
+// Incorporate Mean DOY bands into the collection 
 
 var allcoll = col_mosaic.combine(mean_doy).sort('system:time_start');
 var allcoll_fordiff = col_mosaic.sort('system:time_start');
-print('all collection - mean doy', allcoll);
+print('all collection with mean doy bands', allcoll);
 
 //CLOUD MASK EXCLUDING PIXELS WITH ST_B6 > 300
 function maskL78sr(image) {
@@ -238,7 +229,6 @@ var select_nbr = select_nbr.sort('system:time_start');
 print('Collection with selected bands: ', select_nbr);
 
 //CALCULATE NBR DIFFERENCE: IMAGE(t) - IMAGE(t-1)
-// var nbrbands = replacedVals.select(['NBR']);
 var nbrbands = ltCollection_all_fordiff.select(['NBR']).sort('system:time_start');
 
 var NBRdiff = nbrbands.map(function(img){
@@ -261,27 +251,21 @@ print('NBR diff',NBRdiff);
 
 // Include NBRdiff collection into original collection
 var allcoll_diff = select_nbr.combine(NBRdiff);
-print('all collection - NBR and NBRdiff', allcoll_diff);
+print('all collection with NBR and NBRdiff', allcoll_diff);
 
-//-7.9549, 37.6219 (cena direita)
-//-8.62931, 37.38766 (intersecção)
-// doy 181 -8.9109, 37.13393
-//-8.4632, 37.2161 (area sem queimada incorporada no mapeamento)
-//-8.7058, 37.2805 (ponto do RELATORIO)
-//-9.0513, 39.1904 (area que o pos processamento apagou - fazer serie temporal)
-
-//PLOT TIME SERIES 
+//PLOT TIME SERIES FOR SPECIFIC LAT-LONG
 var point_18 = ee.Geometry.Point([-8.0323, 40.6588]);
-Map.addLayer(point_18,{palette: ['FF8000']}, 'Ponto',false);
+Map.addLayer(point_18,{palette: ['FF8000']}, 'Point - time series chart',false);
 var point_18 = ee.Feature(point_18);
 
-// the plot only works when using metric projections
+// Reproject - the plot only works when using metric projections
 var reproj = function(img) {
   var new_proj = img.reproject('EPSG:32629', null, 30);
   return new_proj;
 };
 var select_nbr_rep = select_nbr.map(reproj);
 
+//NBR chart
 var chart = ui.Chart.image.series({
     imageCollection: select_nbr_rep.select('NBR'),
     region: point_18.geometry()
@@ -297,6 +281,7 @@ print(chart);
 
 var allcoll_diff_rep = allcoll_diff.map(reproj);
 
+//NBR and deltaNBR chart
 var chart = ui.Chart.image.series({
     imageCollection: allcoll_diff_rep.select('NBR','NBRdiff'),
     region: point_18.geometry()
@@ -310,6 +295,7 @@ var chart = ui.Chart.image.series({
     });
 print(chart);
 
+//SWIR2, NIR and Red chart
 var chart = ui.Chart.image.series({
     imageCollection: allcoll_diff_rep.select('SWIR2','NIR','Red'),
     region: point_18.geometry()
@@ -323,7 +309,7 @@ var chart = ui.Chart.image.series({
     });
 print(chart);
 
-//REDUCE BY MINIMUM NBR DIFFERENCE
+//CREATE COMPOSITE IMAGE BASED ON MINIMUM deltaNBR (LARGEST NBR DROP)
 
 var allcoll_diff_select = allcoll_diff.select(['NBRdiff','Red','NIR','SWIR2','Thermal','doy','NBR','meanDOY','DOYbefore']);
 
@@ -332,22 +318,22 @@ var min_NBRdiff = allcoll_diff_select.reduce(ee.Reducer.min(allcoll_diff_select.
 
 var min_NBRdiff = min_NBRdiff.select(['Red','NIR','SWIR2','Thermal','doy','NBR','NBRdiff','meanDOY','DOYbefore']);
 
-print('Image reduced by min NBRdiff and other bands: ', min_NBRdiff);
+print('Image reduced by min deltaNBR and other bands: ', min_NBRdiff);
 
 var min_NBRdiff_pt = min_NBRdiff.clip(geom);
 
-//Import and print Portugal fires
+//Import and print burned areas (Annual Portuguese Fire Atlas)
 var BA = ee.FeatureCollection('users/alanakneves/merge_2016_sup5');
-Map.addLayer(BA, {color: 'FF0000'}, 'BA',false);
+Map.addLayer(BA, {color: 'FF0000'}, 'Burned area',false);
 
+//Clip the minimum deltaNBR by the 2016 burned area limits
 var min_NBRdiff_pt = min_NBRdiff_pt.clip(BA);
 
 Map.addLayer(min_NBRdiff_pt, {bands: ['doy'], min: 0, max: 365}, 'DOY',false);
 Map.addLayer(min_NBRdiff_pt, {bands: ['meanDOY'], min: 0, max: 365}, 'mean DOY');
 Map.addLayer(min_NBRdiff_pt, {bands: ['DOYbefore'], min: 0, max: 365}, 'DOY before',false);
 
-
-
+//Export the most relevant bands to Google Drive (delete /* and */ to run it)
 /*
 //NBRdiff
  Export.image.toDrive({
@@ -360,7 +346,6 @@ Map.addLayer(min_NBRdiff_pt, {bands: ['DOYbefore'], min: 0, max: 365}, 'DOY befo
    fileFormat: 'GeoTIFF',
    crs: 'EPSG:32629'
  });
-
 //NBR
  Export.image.toDrive({
    image:min_NBRdiff_pt.select(['NBR']).toFloat(),
@@ -372,7 +357,6 @@ Map.addLayer(min_NBRdiff_pt, {bands: ['DOYbefore'], min: 0, max: 365}, 'DOY befo
    fileFormat: 'GeoTIFF',
    crs: 'EPSG:32629'
  }); 
-
 //meanDOY
  Export.image.toDrive({
    image:min_NBRdiff_pt.select(['meanDOY']).toUint32(),
@@ -396,8 +380,6 @@ Map.addLayer(min_NBRdiff_pt, {bands: ['DOYbefore'], min: 0, max: 365}, 'DOY befo
    fileFormat: 'GeoTIFF',
    crs: 'EPSG:32629'
  });
-*/
-/*
 //DOYbefore
  Export.image.toDrive({
    image:min_NBRdiff_pt.select(['DOYbefore']).toUint32(),
